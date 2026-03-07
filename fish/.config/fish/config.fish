edbf52f94eecc67b50be20dcf7a102d1bc7e9dea
manolawl <manolawl@proton.me>
2026-03-07 22:31:17 +0800

    added aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,4 +6,28 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+function dots
+  cd ~/dotfiles && git status
+end
+
+function ll
+  ls -A -C -lh --color=auto
+end
+
+function grep
+  rg -p -i
+end
+
+function tr
+  tree -a -C
+end
+
+function ff
+  fastfetch
+end
+
+function firefox
+  zen-browser
+end
+
 starship init fish | source
