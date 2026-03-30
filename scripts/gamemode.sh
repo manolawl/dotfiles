993f4e3e916255ea2fbebe84bbdec42c1e8632d5
manolawl <manolawl+git@proton.me>
2026-03-30 13:18:29 +0800

    minimal config

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -11,13 +11,13 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
     keyword general:gaps_out 0;\
     keyword general:border_size 0;\
     keyword decoration:rounding 0"
-  pkill waybar && waybar -s ~/.config/waybar/minimal-style.css
+  pkill waybar && waybar -s ~/.config/waybar/minimal-style.css -c ~/.config/waybar/minimal-config.jsonc
   hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
   exit
 else
   hyprctl notify 1 5000 "rgb(d20f39)" "Gamemode [OFF]"
   hyprctl reload
-  pkill waybar && waybar -s ~/.config/waybar/style.css
+  pkill waybar && waybar -s ~/.config/waybar/style.css -c ~/.config/waybar/config.jsonc
   exit 0
 fi
 exit 1
