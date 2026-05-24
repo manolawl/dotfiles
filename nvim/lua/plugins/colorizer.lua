1e859a70fcc101f5990924592328f4c7e168fa95
manolawl <manolawl+git@proton.me>
2026-05-24 19:27:33 +0800

    dsafsdaf

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,35 @@
+vim.pack.add({
+	"https://github.com/catgoose/nvim-colorizer.lua"
+})
+-- Enable all CSS color formats
+require("colorizer").setup({
+  options = { parsers = { css = true } },
+})
+
+-- CSS functions only, with virtualtext display
+require("colorizer").setup({
+  options = {
+    parsers = { css_fn = true },
+    display = {
+      mode = "virtualtext",
+      virtualtext = { position = "after" },
+    },
+  },
+})
+
+-- Preset with individual override
+require("colorizer").setup({
+  options = {
+    parsers = { css = true, rgb = { enable = false } },
+  },
+})
+
+-- Per-filetype overrides
+require("colorizer").setup({
+  filetypes = {
+    "*",
+    "!markdown",
+    html = { mode = "foreground" },
+    cmp_docs = { always_update = true },
+  },
+})
